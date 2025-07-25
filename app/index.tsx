051cccef0b1b287aed0f8371900448f21d5cd4dd
Raghib <raghib108@gmail.com>
2025-07-25 19:14:45 +0500

    initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,102 @@
-import { Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-export default function Index() {
+const HomeScreen = () => {
+  const todoData = [
+    {
+      id: 1,
+      title: "Todo 1",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "Todo 2",
+      isDone: false,
+    },
+    {
+      id: 3,
+      title: "Todo 3",
+      isDone: false,
+    },
+    {
+      id: 4,
+      title: "Todo 4",
+      isDone: true,
+    },
+    {
+      id: 5,
+      title: "Todo 5",
+      isDone: false,
+    },
+    {
+      id: 6,
+      title: "Todo 6",
+      isDone: false,
+    },
+  ];
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={style.container}>
+      <View style={style.header}>
+        <TouchableOpacity onPress={() => {}}>
+          <Image
+            source={{ uri: "https://xsgames.co/randomusers/avatar.php?g=male" }}
+            style={{ width: 40, height: 40, borderRadius: 20 }}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={style.searchBar}>
+        <TextInput
+          placeholder="Search"
+          style={style.searchBar}
+          clearButtonMode="always"
+        />
+      </View>
+      <FlatList
+        data={todoData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
     </View>
   );
-}
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5 ",
+    paddingHorizontal: 20,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  searchBar: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 10,
+    gap: 10,
+  },
+  searchInput: {
+    flex: 1,
+    fontSize: 16,
+    color: "#333",
+  },
+});
+
+export default HomeScreen;
